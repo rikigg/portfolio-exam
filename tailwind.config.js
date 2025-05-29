dcc0850a4155a3fe6950e5a9e591eb2bac1762b4
rikigg <rikimain163@gmail.com>
2025-05-30 01:15:09 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Work Sans', 'Noto Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
